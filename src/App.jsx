f09a1a9690453cc73e83bc502886c555efde35b8
Extreme <npyetro26@gmail.com>
2025-03-06 14:21:21 -0300

    Pagina2emudancasnocsspagina

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import Pagina1 from './pages/Pagina1'
+import Pagina2 from './pages/page2'
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
           </Routes>
         
       </BrowserRouter>
-      <Pagina1/>
+      
     </>
   )
 }
