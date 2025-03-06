f3c5b6d49005677445f0d82444f9c7877cbf6255
Lucas C <lucascasagrandesesi@gmail.com>
2025-03-06 11:50:50 -0300

    Criação do componente cards para home

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
-
+import Home from "./pages/Home"
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='' element='' />
+          <Route path='/' element={<Home/>} />
         </Routes>
       </BrowserRouter>
+      <Card/>
+      
     </>
   )
 }
