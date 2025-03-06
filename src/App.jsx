004bded68f62a7da382b751ac1c75dbfac50d335
Milena <mile.26souza08@gmail.com>
2025-03-06 13:48:24 -0300

    Atualizações

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home/>} />  
+          <Route path='/' element={<Home/>} />
+           
           </Routes>
       </BrowserRouter>
     </>
