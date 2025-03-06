590c96226183280bf06aaf0efa3b7f42fdec4d67
Vitor Cecato <vitor.cecatosesi@gmail.com>
2025-03-06 13:55:38 -0300

    Correção de banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,20 +4,14 @@
 
 // Componentes
     import Header from "../components/Header"
-//.
-
-// Fotos
-    // import foto1 from "../assets/imgbanner1.png"
-    // import foto2 from "../assets/imgbanner2.png"
-    // import foto3 from "../assets/imgbanner3.png"
-    // import banner from "../assets/banner.png"
+    import Navbar from "../components/NavBar"
 //.
 
 function Home(){
     return(
         <>
             <Header />
-            
+            <Navbar/>
             <section className="banner">
                 <h1>AS <span>MELHORES</span> <br /> VIAGENS DO <span>MUNDO <br /> INTEIRO</span></h1>
             </section>
