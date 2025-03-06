512923d54c207889d089a19a6f9b2de44c5a9ef3
PietroMelle <mellepietro3@gmail.com>
2025-03-06 15:39:58 -0300

    adicionando o footer e header na pagin 3

--- src/pages/Page3.jsx
+++ src/pages/Page3.jsx
@@ -1,23 +1,28 @@
-import Card from "../components/Card"
+import Card from "../components/Card";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 
-function Page3 () {
-    return (
-      <>
+function Page3() {
+  return (
+    <>
+      <Header />
+
       <center>
-      <Card 
-       Foto="https://www.cnnbrasil.com.br/viagemegastronomia/wp-content/uploads/sites/5/2021/02/comemoracao-disney-e1614010798887.jpg?w=750"
-       Titulo="Disney"
-       Descricao="A Disney é um lugar mágico onde sonhos ganham vida! Com parques temáticos incríveis, personagens encantados 
-       e atrações para todas as idades, oferece experiências inesquecíveis para toda a família. Seja em Orlando, Paris, 
-       Tóquio ou outras localizações, a Disney é um destino perfeito para quem busca diversão, aventura e momentos de pura fantasia."
-       Estrelas="⭐⭐⭐⭐⭐"
-       Valor="Valor: 5.000,00"
-       Proximo='Proximo'
-      />
-  </center>
-      </>
-    );
-  }
-  
-  export default Page3;
-  
+        <Card
+          Foto="https://www.cnnbrasil.com.br/viagemegastronomia/wp-content/uploads/sites/5/2021/02/comemoracao-disney-e1614010798887.jpg?w=750"
+          Titulo="Disney"
+          Descricao="A Disney é um lugar mágico onde sonhos ganham vida! Com parques temáticos incríveis, personagens encantados 
+          e atrações para todas as idades, oferece experiências inesquecíveis para toda a família. Seja em Orlando, Paris, 
+          Tóquio ou outras localizações, a Disney é um destino perfeito para quem busca diversão, aventura e momentos de pura fantasia."
+          Estrelas="⭐⭐⭐⭐⭐"
+          Valor="Valor: 5.000,00"
+          Proximo="Proximo"
+        />
+      </center>
+
+      <Footer />
+    </>
+  );
+}
+
+export default Page3;
