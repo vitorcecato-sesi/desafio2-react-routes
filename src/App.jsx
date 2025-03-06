6f7e6099224cd1d3e52b1ac240f4da3fae19fa3d
Vitor Cecato <vitor.cecatosesi@gmail.com>
2025-03-06 10:03:12 -0300

    Estrutura da página

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='' element='' />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
