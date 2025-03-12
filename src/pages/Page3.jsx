69bbd2496a42563355fca64f123ef300ea03dd08
PietroMelle <mellepietro3@gmail.com>
2025-03-12 09:28:53 -0300

    mudança de texto da disney

--- src/pages/Page3.jsx
+++ src/pages/Page3.jsx
@@ -14,8 +14,7 @@ function Page3() {
           Foto="https://www.cnnbrasil.com.br/viagemegastronomia/wp-content/uploads/sites/5/2021/02/comemoracao-disney-e1614010798887.jpg?w=750"
           Titulo="Disney - Estados Unidos"
           Descricao="A Disney é um lugar mágico onde sonhos ganham vida! Com parques temáticos incríveis, personagens encantados 
-          e atrações para todas as idades, oferece experiências inesquecíveis para toda a família. Seja em Orlando, Paris, 
-          Tóquio ou outras localizações, a Disney é um destino perfeito para quem busca diversão, aventura e momentos de pura fantasia."
+          e atrações para todas as idades, oferece experiências inesquecíveis para toda a família. A Disney é um destino perfeito para quem busca diversão, aventura e momentos de pura fantasia."
           Estrelas="⭐⭐⭐⭐⭐"
           Valor="Valor: 4.895,00"
           Proximo="Proximo"
